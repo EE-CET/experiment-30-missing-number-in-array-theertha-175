94d4b15529cbe311cdb3aa6e684774b0012c379d
theertha-175 <tve24el010@cet.ac.in>
2026-01-31 09:49:32 +0530

    Update MissingNumber.java

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class MissingNumber {
-   
-        // TODO: Read n
-        // TODO: Calculate the expected sum of numbers from 0 to n: (n * (n + 1)) / 2
-        // TODO: Read n integers and calculate the actual sum of the array elements
-        // TODO: The missing number is (expectedSum - actualSum)
-        // TODO: Print the result
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        int expectedSum = n * (n + 1) / 2;
+        System.out.print(expectedSum - sum);
+    }
 }
